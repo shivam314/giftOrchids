d85a73366636037cbf403edd48b090995952503b
shivam <shivam.kumar@nextuple.com>
2024-07-13 16:24:59 +0530

    minor modification for file upload

--- src/main/java/com/giftOrchids/giftOrchids/controller/ProductController.java
+++ src/main/java/com/giftOrchids/giftOrchids/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.giftOrchids.giftOrchids.service.ProductService;
 import com.giftOrchids.giftOrchids.service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,7 +33,7 @@ public class ProductController {
         return productService.getProducts();
     }
 
-    @PostMapping("/addProduct")
+    @PostMapping(value = "/addProduct", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> addProducts(
       @RequestBody Product product,
       @RequestParam("file") MultipartFile file)
