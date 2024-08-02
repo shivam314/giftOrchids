da1c016679ef85cbceb0f19ee0d032d1da2fc875
shivam <shivam.kumar@nextuple.com>
2024-08-02 20:58:54 +0530

    removed vault config

--- src/main/java/com/giftOrchids/giftOrchids/config/StorageConfig.java
+++ src/main/java/com/giftOrchids/giftOrchids/config/StorageConfig.java
@@ -5,22 +5,28 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StorageConfig {
 
-    @Autowired
-    private VaultConfig vaultConfig;
+    @Value("aws.secrets.S3_ACCESS_KEY")
+    private String s3AccessKey;
+
+    @Value("aws.secrets.S3_SECRET_KEY")
+    private String s3SecretKey;
+
+    @Value("aws.secrets.REGION")
+    private String region;
 
     @Bean
     public AmazonS3 s3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(vaultConfig.getS3AccessKey(), vaultConfig.getS3SecretKey());
+        AWSCredentials credentials = new BasicAWSCredentials(s3AccessKey, s3SecretKey);
         return AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(vaultConfig.getRegion()).build();
+                .withRegion(region).build();
     }
 
 }
